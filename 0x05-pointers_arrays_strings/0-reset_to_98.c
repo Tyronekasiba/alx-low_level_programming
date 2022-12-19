6b0f0334b0d8d35dadb7a895af12199c23349cdc
Tyronekasiba <tyronekasiba@gmail.com>
2022-12-19 13:04:00 -0800

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,16 @@
-#include "main.h"
-/**
- * reset_to_98 - update the value of a var using pointers
- * @n: int to update
- * Return: 0
+/*
+ * File: 0-reset_to_98.c
+ * Written by Tyrone
  */
 
-void reset_to_98(int *n);
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
